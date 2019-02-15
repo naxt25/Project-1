4e3e8dadd827ff896dead052d42cdf085e44e579
Nate <nateaxt@gmail.com>
2019-02-15 11:52:58 -0800

    First Version of Swift App - Have running box/list/button/label

--- Project 1/ViewController.swift
+++ Project 1/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Dwarf", "Gnome", "Halfling", "Elf", "Dragonborn", "Human", "Tebaxi", "Goliath"]
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(list.count)
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var WhatAnAppLabel: UILabel!
+    
+    @IBAction func PressMeButton(_ sender: Any) {
+        view.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
+        WhatAnAppLabel.text = "Nat20!"
+        WhatAnAppLabel.font = UIFont.systemFont(ofSize: 30)
+        WhatAnAppLabel.textAlignment = .left
+    }
+    
 }
 
